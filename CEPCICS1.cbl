9584e33584d842ae9973340a24d3fbe214ff4f53
valtsiq <valtsiq@gmail.com>
2022-11-23 10:04:22 -0300

    Acerto de Cobol

--- CEPCICS1.cbl
+++ CEPCICS1.cbl
@@ -15,83 +15,95 @@
       * Transacao CICS  ...... KEP0
       *
       ******************************************************************
-       identification division.
-       program-id.    cepcics1.
-       environment    division.
-       configuration  section.
-       data           division.
-       working-storage section.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.    CEPCICS1.
+       ENVIRONMENT    DIVISION.
+       CONFIGURATION  SECTION.
+       DATA           DIVISION.
+       WORKING-STORAGE SECTION.
       *----------------------------------------------------------------*
 
-       01 working-all-fills.
-          03 wk-cepv0001-rec.
-             05 wk-cepv0001-code              PIC  X(008).
-             05 wk-cepv0001-uf                PIC  X(002).
-             05 wk-cepv0001-cidade            PIC  X(030).
-             05 wk-cepv0001-bairro            PIC  X(030).
-             05 wk-cepv0001-logradouro        PIC  X(030).
-          03 filler  pic x(009) value         '---------'.
-          03 wk-kep0td-rec.
-             05 wk-kep0td-rec-cpy             pic  x(100).
-             05 wk-kep0td-rec-msg             pic  x(032).
+       01 WORKING-ALL-FILLS.
+          03 WK-CEPV0001-REC.
+             05 WK-CEPV0001-CODE              PIC  X(008).
+             05 WK-CEPV0001-UF                PIC  X(002).
+             05 WK-CEPV0001-CIDADE            PIC  X(030).
+             05 WK-CEPV0001-BAIRRO            PIC  X(030).
+             05 WK-CEPV0001-LOGRADOURO        PIC  X(030).
+          03 FILLER  PIC X(009) VALUE         '---------'.
+          03 WK-KEP0TD-REC.
+             05 WK-KEP0TD-REC-CPY             PIC  X(100).
+             05 WK-KEP0TD-REC-MSG             PIC  X(032).
       *----------------------------------------------------------------*
       *   GENERIC WORK VARIABLES                                       *
       *----------------------------------------------------------------*
-          03 wk-eibresp                       PIC S9(9) COMP-5 SYNC.
+          03 WK-EIBRESP                       PIC S9(9) COMP-5 SYNC.
       *----------------------------------------------------------------*
 
       ******************************************************************
       *    L I N K A G E   S E C T I O N
       ******************************************************************
-       linkage section.
+       LINKAGE SECTION.
 
-       01 dfhcommarea.
-          03 lk-cep               pic  X(008).
-          03 lk-uf                PIC  X(002).
-          03 lk-cidade            PIC  X(030).
-          03 lk-bairro            PIC  X(030).
-          03 lk-logradouro        PIC  X(030).
+       01 DFHCOMMAREA.
+          03 LK-CEP               PIC  X(008).
+          03 LK-UF                PIC  X(002).
+          03 LK-CIDADE            PIC  X(030).
+          03 LK-BAIRRO            PIC  X(030).
+          03 LK-LOGRADOURO        PIC  X(030).
 
       ******************************************************************
       *    P R O C E D U R E S
       ******************************************************************
-       procedure division.
+       PROCEDURE DIVISION.
 
       *----------------------------------------------------------------*
-       mainline section.
+       MAINLINE SECTION.
       *----------------------------------------------------------------*
       * Common code                                                    *
       *----------------------------------------------------------------*
 
       *    move dfhcommarea to wk-cepv0001-rec
 
-           move spaces to wk-cepv0001-rec
-           move spaces to wk-kep0td-rec
+           MOVE SPACES TO WK-CEPV0001-REC
+           MOVE SPACES TO WK-KEP0TD-REC
 
-           exec cics read file('CEPVSA01')
-                          ridfld(lk-cep)
-                          keylength(length of lk-cep)
-                          into(wk-cepv0001-rec)
-                          resp(wk-eibresp)
-           end-exec
+           EXEC CICS READ FILE('CEPVSA01')
+                          RIDFLD(LK-CEP)
+                          KEYLENGTH(8)
+                          INTO(WK-CEPV0001-REC)
+                          RESP(WK-EIBRESP)
+           END-EXEC
 
-           if wk-eibresp not equal zeros
-              move lk-cep
-                   to wk-cepv0001-code
-              move 'CEP não encontrado       '
-                   to wk-cepv0001-cidade
-           end-if
-           move spaces          to dfhcommarea
-           move wk-cepv0001-rec to dfhcommarea
+           IF WK-EIBRESP NOT EQUAL ZEROS
+              MOVE LK-CEP
+                   TO WK-CEPV0001-CODE
+              MOVE 'CEP não encontrado       '
+                   TO WK-CEPV0001-CIDADE
+           END-IF
+      *     IF LK-CEP EQUAL '09041160'
+      *        MOVE LK-CEP
+      *             TO WK-CEPV0001-CODE
+      *        MOVE 'CEP nao encontrado       '
+      *             TO WK-CEPV0001-CIDADE
+      *        MOVE 'CEP nao encontrado       '
+      *             TO WK-CEPV0001-BAIRRO
+      *        MOVE 'CEP nao encontrado       '
+      *             TO WK-CEPV0001-LOGRADOURO
+      *        MOVE SPACES
+      *             TO WK-CEPV0001-UF
+      *     END-IF
+           MOVE SPACES          TO DFHCOMMAREA
+           MOVE WK-CEPV0001-REC TO DFHCOMMAREA
 
-           move wk-cepv0001-rec to wk-kep0td-rec-cpy
+           MOVE WK-CEPV0001-REC TO WK-KEP0TD-REC-CPY
 
-           exec cics writeq td queue('KEP0')
-                          from (wk-kep0td-rec)
-                          length(length of wk-kep0td-rec)
-                          resp(wk-eibresp)
-           end-exec
+           EXEC CICS WRITEQ TD QUEUE('KEP0')
+                          FROM (WK-KEP0TD-REC)
+                          LENGTH(LENGTH OF WK-KEP0TD-REC)
+                          RESP(WK-EIBRESP)
+           END-EXEC
 
-           exec cics return
-           end-exec
+           EXEC CICS RETURN
+           END-EXEC
            .
