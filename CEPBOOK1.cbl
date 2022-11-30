22875c5574354443aa36d5265ec8fd7ccecf9772
valtsiq <valtsiq@gmail.com>
2022-11-30 10:47:28 -0300

    CLEAR

--- CEPBOOK1.cbl
+++ CEPBOOK1.cbl
@@ -1,6 +1,6 @@
-       01 dfhcommarea.
-          03 lk-cep               pic  x(008).
-          03 lk-uf                PIC  X(002).
-          03 lk-cidade            PIC  X(030).
-          03 lk-bairro            PIC  X(030).
-          03 lk-logradouro        PIC  X(030).
+       01 DFHCOMMAREA.
+          03 CEP               PIC  X(008).
+          03 UF                PIC  X(002).
+          03 CIDADE            PIC  X(030).
+          03 BAIRRO            PIC  X(030).
+          03 LOGRADOURO        PIC  X(030).
